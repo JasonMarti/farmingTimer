f269f5e8800dcee38bbe89f722a9e14901fb40e0
JasonMarti <JasonMartindevelopment@gmail.com>
2018-10-17 13:56:48 -0700

    end of lunch

--- FarmTimer.cpp
+++ FarmTimer.cpp
@@ -8,10 +8,12 @@ this->plantType = plant;
 bool FarmTimer::calculateEndTime(){
     int todSeconds = time(NULL) % 86400;
     int todMinutes = todSeconds / 60;
+    int minuteOfNextGrowthCycle = 0;
+    int days = 0;
     switch (plantType) {
         case Oak:
-
-
+        minuteOfNextGrowthCycle = (todMinutes % TREE_GROWTH_CYCLE) * TREE_GROWTH_CYCLE + TREE_GROWTH_CYCLE;
+        if(minuteOfNextGrowthCycle)
         break;
         case Willow:
         break;
